97959700c3484e0573eb64bf36a3a04bf47553f9
Veronika Sesay <veronika.sesay@gmail.com>
2022-08-18 21:53:51 +0200

    Contact form layout

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,26 +4,41 @@ import { useOutletContext } from 'react-router-dom';
 
 export const Home = () => {
   const [langVersion, setLangVersion] = useOutletContext();
+
   const [contactMe, setContactMe] = useState(false);
-  const openContactForm = () => setContactMe(true);
+  const [submitForm, setSubmitForm] = useState(false);
 
   let content = {
     English: {
-      intro: 'Hello',
+      intro:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. At mollitia facere nam dolorum vero natus, aut iusto rerum cumque quos esse illo quas modi aspernatur maxime consequuntur, nihil eum laborum!',
+      formName: 'Name',
+      email: 'E-mail',
+      contactBtn: 'Contact me',
+      message: 'Your message',
+      submitBtn: 'Send message',
+      submitMessage:
+        'Thank you for contacting me, I will reply to you shortly. Have a nice day!',
     },
     Czech: {
       intro: 'Ahoj',
+      formName: 'Jméno',
+      email: 'E-mail',
+      contactBtn: 'Kontaktujte mě',
+      message: 'Vaše zpráva',
+      submitBtn: 'Odeslat',
+      submitMessage:
+        'Děkuji za Vaši zprávu, ozvu se Vám co nejdříve. Hezký den!',
     },
   };
 
-  console.log(langVersion, 'langVersion pres outlet');
   langVersion === 'Czech'
     ? (content = content.Czech)
     : (content = content.English);
 
   return (
     <>
-      <div className="intro">
+      <div className={contactMe ? 'intro intro--hide' : 'intro'}>
         <video className="intro__logo" autoPlay muted>
           <source src="/img/logo-video.mp4" type="video/mp4" />
           Your browser does not support the video tag.
@@ -42,8 +57,40 @@ export const Home = () => {
         </div>
       </div>
       <div className="contact-form">
-        <button>Kontaktujte mě</button>
-        <form action=""></form>
+        <button
+          onClick={() => setContactMe(!contactMe)}
+          className="contact-form__btn"
+        >
+          {content.contactBtn}
+        </button>
+        <form
+          className={
+            contactMe ? 'contact-form' : 'contact-form contact-form--closed'
+          }
+          action=""
+        >
+          <label htmlFor="name">
+            {content.formName}
+            <input type="text" name="name" id="name" size="60" />
+          </label>
+          <label htmlFor="email">
+            {content.email}
+            <input type="email" name="email" id="email" size="60" />
+          </label>
+          <label htmlFor="message">
+            {content.message}
+            <textarea
+              name="message"
+              id="message"
+              cols="50"
+              rows="10"
+            ></textarea>
+          </label>
+          <button onClick type="submit" className="contact-form__btn">
+            {content.submitBtn}
+          </button>
+          <div className="submit-message"></div>
+        </form>
       </div>
     </>
   );
